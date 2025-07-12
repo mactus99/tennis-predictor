1c5ac5dbd8fe93978153bb45bf4a810f516194fd
mactus99 <matteo.danella5@gmail.com>
2025-07-12 04:07:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import pandas as pd
 import numpy as np
 import streamlit.components.v1 as components
 
-# Inietta lo script di Google Analytics
-components.html("""
+GA_SCRIPT = """
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-2FEMB1XB39"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-2FEMB1XB39');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2FEMB1XB39');
 </script>
-""", height=0)
+"""
+
+components.html(GA_SCRIPT, height=0)
 
 # Versione: aggiorna ogni volta che modifichi il codice
 APP_VERSION = "2025-07-12-live-set-v1.5.6"
